d3f66e6105bcf63a6e8d25415de1804375afdce0
cbonoz <chrisdistrict@gmail.com>
2019-08-25 11:23:24 -0400

    url

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -38,6 +38,12 @@ export default class Home extends Component {
   resultClicked = (result) => {
     // TODO: open result in new tab window.
     console.log('clicked', result)
+    const { name, url } = result
+    if (url) {
+      window.open(url, "_blank")
+    } else {
+      alert(`'No additional information found for ${name}.`)
+    }
   }
 
   _fetch = q => {
@@ -101,15 +107,13 @@ export default class Home extends Component {
                     placeholder="Describe your ideal charity with phrases... <3"
                   />
 
-                  {!loading && <a class="button is-danger search-button" onClick={() => this.autocompleteSearch(q)}>Search</a>}
+                  {!loading && <a className="button is-danger search-button" onClick={() => this.autocompleteSearch(q)}>Search</a>}
 
                   </div>
                 </div>
               <hr/>
               <div className="main-results">
-                {loading && <div>
-                  <img src={loading_spinner}/>
-                  </div>}
+                {loading && <div><img src={loading_spinner}/></div>}
 
                 {hasResults && !loading && <p className='has-text-centered result-info-text'>
                   Showing top <b>{results.length}</b> results. Charity matching took <b>{duration}</b> seconds.
@@ -124,15 +128,13 @@ export default class Home extends Component {
                
                 {(!loading && _searches.length > 0) && <div className="past-searches">
                   <hr />
-                    <a class="button is-danger clear-button is-outlined" onClick={event => this.setState({ _searches: [] })}>
+                    <a className="button is-danger clear-button is-outlined" onClick={event => this.setState({ _searches: [] })}>
                       Clear history
                     </a>
-                    <ol>
                     <p>Recent Searches:</p>
                     {_searches.map((s, i) => {
-                      return <li key={s + i}>{s}</li>
+                      return <div key={s + i}><b>{i + 1}.</b> {s}</div>
                     })}
-                    </ol>
                 </div>}
               </div>
             </div>
